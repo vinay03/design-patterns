e80eb44e569bb9b67bec930dadc076b90cc62079
Vinay Jeurkar <vjeurkar03@gmail.com>
2024-01-04 09:09:10 +0530

    Added basic example for singleton pattern

---
+++ Singleton/Example1/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"sync"
+)
+
+type singletonDatabase struct {
+	capitals map[string]int
+}
+
+func (db *singletonDatabase) GetPopulation(name string) int {
+	return db.capitals[name]
+}
+
+func readData(path string) (map[string]int, error) {
+	// ex, err := os.Executable()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// exPath := filepath.Dir(ex)
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	result := map[string]int{}
+
+	for scanner.Scan() {
+		k := scanner.Text()
+		scanner.Scan()
+		v, _ := strconv.Atoi(scanner.Text())
+		result[k] = v
+	}
+
+	return result, nil
+}
+
+var once sync.Once
+var instance *singletonDatabase
+
+func GetSingletonDatabase() *singletonDatabase {
+	once.Do(func() {
+		caps, e := readData("./capitals")
+		db := singletonDatabase{caps}
+		if e == nil {
+			db.capitals = caps
+		} else {
+			fmt.Println(e)
+		}
+		instance = &db
+	})
+	return instance
+}
+
+func GetTotalPopulation(cities []string) int {
+	result := 0
+	for _, city := range cities {
+		result += GetSingletonDatabase().GetPopulation(city)
+	}
+	return result
+}
+
+func main() {
+	db := GetSingletonDatabase()
+	pop := db.GetPopulation("Seoul")
+	fmt.Println("Pop of Seoul = ", pop)
+
+	cities := []string{"Seoul", "Mexico City"}
+	tp := GetTotalPopulation(cities)
+	ok := tp == (17500000 + 17400000)
+	fmt.Println(ok)
+}
