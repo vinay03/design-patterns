fcc06f0e4daff7b99ef6594e03702b47ac0eba1f
Vinay Jeurkar <vjeurkar03@gmail.com>
2024-01-05 07:50:05 +0530

    Added example for adapter pattern

---
+++ 21_Adapter/Example1/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func minmax(a, b int) (int, int) {
+	if a < b {
+		return a, b
+	} else {
+		return b, a
+	}
+}
+
+type Line struct {
+	X1, Y1, X2, Y2 int
+}
+
+type VectorImage struct {
+	Lines []Line
+}
+
+func NewRectangle(width, height int) *VectorImage {
+	width -= 1
+	height -= 1
+	return &VectorImage{[]Line{
+		{0, 0, width, 0},
+		{0, 0, 0, height},
+		{width, 0, width, height},
+		{0, height, width, height}}}
+}
+
+type Point struct {
+	X, Y int
+}
+
+type RasterImage interface {
+	GetPoints() []Point
+}
+
+func DrawPoints(owner RasterImage) string {
+	maxX, maxY := 0, 0
+	points := owner.GetPoints()
+	for _, pixel := range points {
+		if pixel.X > maxX {
+			maxX = pixel.X
+		}
+		if pixel.Y > maxY {
+			maxY = pixel.Y
+		}
+	}
+	maxX += 1
+	maxY += 1
+
+	// preallocate
+
+	data := make([][]rune, maxY)
+	for i := 0; i < maxY; i++ {
+		data[i] = make([]rune, maxX)
+		for j := range data[i] {
+			data[i][j] = ' '
+		}
+	}
+
+	for _, point := range points {
+		data[point.Y][point.X] = '*'
+	}
+
+	b := strings.Builder{}
+	for _, line := range data {
+		b.WriteString(string(line))
+		b.WriteRune('\n')
+	}
+
+	return b.String()
+}
+
+type VectorToRasterAdapter struct {
+	points []Point
+}
+
+func (v VectorToRasterAdapter) GetPoints() []Point {
+	return v.points
+}
+func (a *VectorToRasterAdapter) addLine(line Line) {
+	left, right := minmax(line.X1, line.X2)
+	top, bottom := minmax(line.Y1, line.Y2)
+	dx := right - left
+	dy := line.Y2 - line.Y1
+
+	if dx == 0 {
+		for y := top; y <= bottom; y++ {
+			a.points = append(a.points, Point{left, y})
+		}
+	} else if dy == 0 {
+		for x := left; x <= right; x++ {
+			a.points = append(a.points, Point{x, top})
+		}
+	}
+
+	fmt.Println("generated", len(a.points), "points")
+}
+func VectorToRaster(vi *VectorImage) RasterImage {
+	adapter := VectorToRasterAdapter{}
+	for _, line := range vi.Lines {
+		adapter.addLine(line)
+	}
+	return adapter
+}
+
+func main() {
+	rc := NewRectangle(6, 4)
+	a := VectorToRaster(rc)
+	_ = VectorToRaster(rc)
+	fmt.Println(DrawPoints(a))
+}
