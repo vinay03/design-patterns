7b3ce763cfa76aec1fcfeb5732f5f23b4cf6f88d
Vinay Jeurkar <vjeurkar03@gmail.com>
2024-01-05 08:14:54 +0530

    Added example for Bridge pattern

---
+++ 22_Bridge/Basic/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+// Circle, square
+// Raster, vector
+
+//
+
+type Renderer interface {
+	RenderCircle(radius float32)
+}
+
+type VectorRenderer struct {
+	//
+}
+
+func (v *VectorRenderer) RenderCircle(radius float32) {
+	fmt.Println("Drawing ca circle of radius", radius)
+}
+
+type RasterRenderer struct {
+	Dpi int
+}
+
+func (r *RasterRenderer) RenderCircle(radius float32) {
+	fmt.Println("Drawing pixels for circle of radius", radius)
+}
+
+type Circle struct {
+	renderer Renderer
+	radius   float32
+}
+
+func NewCirlcle(renderer Renderer, radius float32) *Circle {
+	return &Circle{renderer: renderer, radius: radius}
+}
+
+func (c *Circle) Draw() {
+	c.renderer.RenderCircle(c.radius)
+}
+
+func (c *Circle) Resize(factor float32) {
+	c.radius *= factor
+}
+
+func main() {
+	raster := RasterRenderer{}
+	// vector := VectorRenderer{}
+	circle := NewCirlcle(&raster, 5)
+	circle.Draw()
+	circle.Resize(2)
+	circle.Draw()
+}
